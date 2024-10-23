9a7034b0969bae41fca2266176dde878fd0a1c3f
Fabian <110415215+LetsBePhoenix@users.noreply.github.com>
2024-10-23 16:18:03 +0200

    Update cPlayer.py

--- cPlayer.py
+++ cPlayer.py
@@ -1,10 +1,76 @@
-class Player:
-    def __init__(self, exp: int, hp: int, mp: int, armor: Armor):
-        self.exp = exp
-        self.hp = hp
-        self.mp = mp
-        self.armor = armor
+import json
+import os
 
-    def __repr__(self):
-        return (f"Player(Exp: {self.exp}, HP: {self.hp}, MP: {self.mp}, "
-                f"Armor: {self.armor})")
+
+# Klasse SPIELER
+class Player:
+    def __init__(self, playerID, playerName):
+        self.playerID = str(playerID)
+        self.playerName = str(playerName)
+        self.lvl = 1
+        self.hp = 10
+        self.mp = 20
+        self.exp = 0
+        self.stage_max = 1
+        self.stage = 3
+        self.load_player()
+
+    # Spieler-Daten aus JSON laden
+    def load_player(self):
+        file_path = f"data\\dataPlayer\\{self.playerID}.json"
+        if os.path.exists(file_path):
+            with open(file_path, "r") as file:
+                data = json.load(file)
+                if "stats" in data:
+                    self.hp = data["stats"]["hp"]
+                    self.mp = data["stats"]["mp"]
+                    self.lvl = data["stats"]["lvl"]
+                    self.exp = data["stats"]["exp"]
+                    self.stage = data["stats"]["stage"]
+                    self.stage_max = data["stats"]["stage_max"]
+        else:
+            # Spieler existiert nicht, daher neue Datei mit Standardwerten erstellen
+            self.save_player()
+
+            # Füge spieler in die playerLibrary hinzu
+            library_file_path = "data\\dataLibrary\\playerLibrary.json"
+            if os.path.exists(library_file_path):
+                with open(library_file_path, "r") as file:
+                    try:
+                        data = json.load(file)
+                    except json.JSONDecodeError:
+                        data = {}  # Falls die Datei leer oder beschädigt ist
+            else:
+                data = {}
+            data[self.playerID] = self.playerName
+            with open(library_file_path, "w") as file:
+                json.dump(data, file, indent=4)
+
+    def save_player(self):
+        if os.path.exists(f"data\\dataPlayer\\{self.playerID}.json"):
+            with open(f"data\\dataPlayer\\{self.playerID}.json", "r") as file:
+                data = json.load(file)
+        else:
+            data = {}
+
+        data["stats"] = {
+            "hp": self.hp,
+            "mp": self.mp,
+            "lvl": self.lvl,
+            "exp": self.exp,
+            "stage": self.stage,
+            "stage_max": self.stage_max
+        }
+
+        with open(f"data\\dataPlayer\\{self.playerID}.json", "w") as file:
+            json.dump(data, file, indent=4)
+
+    def try_levelup(self, lvl):
+        level_file_path = f"data\\dataLibrary\\playerLevel.json"
+        with open(level_file_path, "r") as file:
+            data = json.load(file)
+            nextLevel = f"lvl{lvl}"
+
+            if int(data[nextLevel]) <= self.exp:
+                self.exp -= data[nextLevel]
+                self.lvl += 1
